dd11fc23116e87bc5411846ff609d32943870efe
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2022-02-03 22:56:06 +0300

    First commit

---
+++ StartBx/frontend/views.py
@@ -0,0 +1,7 @@
+from re import template
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
