712cdd9cb0038cef629b4d0b4def19105dc37931
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2022-02-20 19:01:56 +0300

    Edited service section design in the homepage

--- StartBx/StartBx/settings.py
+++ StartBx/StartBx/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lvqz#lt2q#^m92o1=&_bpqx_ogzi0ru*jni26(f4m8+zddvo6u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
