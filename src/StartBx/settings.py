8e045d86849d5d25b5fa3b8514a23f5f065cee5a
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2022-05-12 22:31:49 +0300

    Added domain to allowed host

--- src/StartBx/settings.py
+++ src/StartBx/settings.py
@@ -34,8 +34,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 # Application definition
 
