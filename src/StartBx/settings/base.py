c3b786fe63a84930476d750c6a033115d34b0aab
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2022-05-18 23:23:51 +0300

    Edit STATIC in base,py

--- src/StartBx/settings/base.py
+++ src/StartBx/settings/base.py
@@ -147,13 +147,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+if DEBUG:
+    STATIC_URL = 'static/'
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+else:
+    STATIC_URL = 'static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
