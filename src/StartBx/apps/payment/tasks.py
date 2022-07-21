35528766b8d78c501decaf46b94ce886c0ab15d7
hellen17 <kokachhellen@gmail.com>
2022-07-21 22:54:40 +0300

    attach pdf file to email

--- src/StartBx/apps/payment/tasks.py
+++ src/StartBx/apps/payment/tasks.py
@@ -1,4 +1,6 @@
 from io import BytesIO
+import os
+from StartBx.settings.base import BASE_DIR
 from celery import shared_task
 import weasyprint
 from django.template.loader import render_to_string
@@ -17,7 +19,7 @@ def payment_completed(order_id):
     order = Order.objects.get(id=order_id)
     # create invoice e-mail
     subject = f"StartBox - EE Invoice no. {order.id}"
-    message = "Please, find attached the invoice for your recent purchase."
+    message = "Please, find attached your template and the invoice for your recent purchase."
     email = EmailMessage(subject, message, "hellen@impactafrica.network", [order.email])
     # generate PDF
     html = render_to_string("orders/order/pdf.html", {"order": order})
@@ -26,7 +28,7 @@ def payment_completed(order_id):
 
     # attach PDF file
     email.attach(f"order_{order.id}.pdf", out.getvalue(), "application/pdf")
-    
+    email.attach_file('/home/hellen/Documents/projects/IAN/StartBx/src/StartBx/templates/documents/NDA.pdf')
     # send e-mail
     email.send()
 
